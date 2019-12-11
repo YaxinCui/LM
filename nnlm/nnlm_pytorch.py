695d3b9798111710fc7549c7ae2dbed9d3fb58a6
YaxinCui <cui_yaxin@outlook.com>
2019-12-11 21:56:40 +0800

    修正了pytorch版NNLM损失值不下降问题，并做了不同初始化方式、损失函数、截断梯度的对比实验。

--- nnlm/nnlm_pytorch.py
+++ nnlm/nnlm_pytorch.py
@@ -2,22 +2,29 @@ from input_data import *
 from torch import nn, optim
 from torch.autograd import Variable
 
-from input_data import *
-
-import numpy as numpy
+import numpy as np
 import argparse
 import time
 import math
 import torch
 
+# 环境，pytorch1.3
 # 参考，https://github.com/Vincentkitty/NNLM_by_pytorch/blob/master/NNLM/NNLM.py
 # 参考，https://github.com/lsvih/nnlm
 # 参考，https://www.jianshu.com/p/be242ed3f314
 # 参考，https://github.com/L1aoXingyu/pytorch-beginner/blob/master/06-Natural%20Language%20Process/N-Gram.py
 
+# 总结
+# 之前使用Softmax的损失值一直在8.5，一直降不下来，直到使用LogSoftmax（）才下降到4.5左右，在目标比较稀疏的时候，使用LogSoftmax效果更好
+# 除了损失函数使用Softmax外，参数的初始化对损失值下降也有很大影响
+# 对embeds的初始值设置为正态分布，比均匀分布梯度下降得更快
+# 建议使用grad_clip，使得梯度截断不是很大
+# batch_size设置太多，也会影响梯度下降速度
+
+# debug过程，删去tensorflow版nnlm的各种优化，可以得到各种优化效果到底是什么效果
 
 class NNLM(nn.Module):
-    def __init__(self, vocab_size, word_dim, win_size, hidden_num):
+    def __init__(self, vocab_size, word_dim, win_size, hidden_num, grad_clip):
         # 单词个数，词向量长度，窗口大小，隐藏层数量
         super(NNLM, self).__init__()
         # 将词映射成向量
@@ -26,37 +33,39 @@ class NNLM(nn.Module):
         self.win_size = win_size
         self.hidden_num = hidden_num
 
-        self.embeds = nn.Embedding(vocab_size, word_dim)    # embeds [vocab_size, word_dim]
-        torch.nn.init.uniform(self.embeds.weight, a=-2, b=2)
-
+        self.embeds = nn.Embedding(vocab_size, word_dim)    # embeds [vocab_size, word_dim] ，不能有max_norm，否则报错
+    #    torch.nn.init.uniform(self.embeds.weight, a=-1, b=1)
+        torch.nn.init.normal(self.embeds.weight) # 使用正态分布，梯度下降得更快
+        
         self.layer1 = nn.Sequential(
                 nn.Linear(win_size * word_dim, hidden_num),
                 nn.Tanh()
             )
             # []
         
-        self.layer2 = nn.Sequential(
-                nn.Linear(hidden_num, vocab_size),
-                nn.Softmax()
-            )
-#        torch.nn.init.normal(self.layer1[0].weight, std=1.0 / math.sqrt(hidden_num))
-#        torch.nn.init.normal(self.layer1[0].bias, std=1.0 / math.sqrt(hidden_num))
-#        torch.nn.init.normal(self.layer2[0].weight, std=1.0 / math.sqrt(win_size * word_dim))
-#        torch.nn.init.normal(self.layer2[0].bias, std=1.0 / math.sqrt(hidden_num))
+        self.layer2 = nn.Linear(hidden_num, vocab_size)
+        self.layer3 = nn.Linear(win_size * word_dim, vocab_size)
+        self.layer4 = nn.LogSoftmax(dim=1)
+        a = [win_size * word_dim * hidden_num]
+        
+        """
+        # 有没有初始化，收敛速度差不多
+        nn.init.normal(self.layer1[0].weight/(torch.sqrt(torch.Tensor(a))))
+        a = [hidden_num * vocab_size]
+        nn.init.normal(self.layer2.weight/(torch.sqrt(torch.Tensor(a))))
+        a = [win_size * word_dim * vocab_size]
+        nn.init.normal(self.layer3.weight/(torch.sqrt(torch.Tensor(a))))
+        """
+
+#        self.layer4 = nn.LogSoftmax()
 
     def forward(self, x):
         x = self.embeds(x)
         x = x.view(-1, self.win_size * self.word_dim)
 #        x = x.view(-1, 1)
-        out = self.layer1(x)
-        out = self.layer2(out)
-#        print("weight:", self.embeds.weight[0][:20])
-#        embeddings_norm = torch.sqrt(torch.sum(torch.mul(self.embeds.weight, self.embeds.weight), dim=1))
-        #print("embed shape", embeddings_norm.shape)
-        #print("embed type", embeddings_norm.type)
-        #print("weight shape", self.embeds.weight.shape)
-        #print("weight type", self.embeds.weight.type)
-#        self.embeds.from_pretrained(self.embeds.weight / embeddings_norm.unsqueeze(1))
+        hidden = self.layer1(x)
+        out = self.layer2(hidden) + self.layer3(x)
+        out = self.layer4(out)
         
         return out
         
@@ -65,7 +74,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='nnlm/data/',
                        help='data directory containing input.txt')
-                       
     parser.add_argument('--data_name', type=str, default='input.en.txt',
                        help='data directory containing input.en.txt')
     parser.add_argument('--batch_size', type=int, default=15,
@@ -87,7 +95,7 @@ def main():
     args.vocab_size = data_loader.vocab_size
 
     criterion = nn.CrossEntropyLoss()
-    model = NNLM(args.vocab_size, args.word_dim, args.win_size, args.hidden_num)
+    model = NNLM(args.vocab_size, args.word_dim, args.win_size, args.hidden_num, args.grad_clip)
 
     if torch.cuda.is_available():
         model = model.cuda()
@@ -102,24 +110,27 @@ def main():
             x, target = data_loader.next_batch()
             x = torch.from_numpy(x).long()
             target = torch.from_numpy(target.squeeze()).long()
+            
+            if torch.cuda.is_available():
+                x = x.cuda()
+                target = target.cuda()
+
             output = model(x)
-
-
-            #print("output  ", output[10:20], " \n shape ", output.shape)
-            #print("target ", target[10:20], " \n shape ", target.shape)
             
             train_loss = criterion(output, target)
-            end = time.time()
+            # 截断梯度，正则化，防止梯度爆炸
+            nn.utils.clip_grad_norm(model.parameters(), max_norm=args.grad_clip, norm_type=2)
+
             optimizer.zero_grad()
             train_loss.backward()
             optimizer.step()
-
+            end = time.time()
             print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}".format(
                         b, data_loader.num_batches,
                         e, train_loss, end - start))
-        # 保存词向量至nnlm_word_embeddings.npy文件
-        np.save('nnlm_word_embeddings.en', model.embeds.weight.detach().numpy())
-
+        # 保存词向量
+        embeds = model.embeds.weight.detach().numpy()
+        np.save(args.data_dir+"nnlm_"+str(args.data_name).replace(".txt", "") + ".npy", embeds)
 
 
 if __name__ == '__main__':
